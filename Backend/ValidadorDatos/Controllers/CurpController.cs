c3367eefe450fb996a7b33f01885f75d664aad57
Carlos Baez <jbaez@outlook.com>
2022-10-08 22:40:52 -0500

    validador de ines, curp
    servicios de gobierno

---
+++ Backend/ValidadorDatos/Controllers/CurpController.cs
@@ -0,0 +1,198 @@
+﻿using AltergoAPI.Nss.Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AltergoAPI.Nss.Core.Controllers
+{
+    [SwaggerTag("APIS RENAPO")]
+    [Produces("application/json")]
+    [Route("Renapo")]
+    [ApiController]
+    public class CurpController : ControllerBase
+    {
+        private const string agente = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50";
+
+        private readonly IConfiguration _configuration;
+
+        public CurpController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Valida la clave única de registro de población de una persona y devuelve sus datos
+        /// </summary>
+        /// <param name="curp" example="HEBG830708HDFRSR03">Clave Única de Registro de Población a 18 caracteres alfanuméricos</param>
+        /// <returns>Datos de la persona registrada con esa CURP</returns>
+        /// <response code="200">Respuesta Valida</response>
+        /// <response code="204">Respuesta Inválida: Sin contenido</response>
+        /// <response code="400">Respuesta Inválida: El curp no tiene el formato correcto</response>
+        /// <response code="404">Respuesta Inválida: No fue posible validar el curp</response>
+        [HttpGet("ValidarCurp")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> ValidarCurpRenapo([FromQuery, Required] string curp)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(curp))
+                {
+                    const string err = "El CURP es requerido.";
+                    return BadRequest(err);
+                }
+
+                const string pattern = @"^([A-Z&]|[a-z&]{1})([AEIOU]|[aeiou]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([HM]|[hm]{1})([AS|as|BC|bc|BS|bs|CC|cc|CS|cs|CH|ch|CL|cl|CM|cm|DF|df|DG|dg|GT|gt|GR|gr|HG|hg|JC|jc|MC|mc|MN|mn|MS|ms|NT|nt|NL|nl|OC|oc|PL|pl|QT|qt|QR|qr|SP|sp|SL|sl|SR|sr|TC|tc|TS|ts|TL|tl|VZ|vz|YN|yn|ZS|zs|NE|ne]{2})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([^A|a|E|e|I|i|O|o|U|u]{1})([A-Z]|[a-z]|[0-9]{1})([0-9]{1})$";
+                var m = Regex.Match(curp, pattern, RegexOptions.IgnoreCase);
+
+                if (!m.Success)
+                {
+                    const string err = "El CURP no tiene el formato correcto";
+                    return BadRequest(err);
+                }
+
+                var client = new RestClient($"{_configuration["Renapo:url"]}/consultaCurp")
+                {
+                    Timeout = -1,
+                    UserAgent = agente
+                };
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                var body = "{\"cveCurp\": \"" + curp.ToUpperInvariant().Trim() + "\"}";
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
+                var response = await client.ExecuteAsync(request);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var curpData = FoByCurpOnly.FromJson(response.Content);
+                    if (curpData.StatusOper.ToUpperInvariant() == "EXITOSO")
+                    {
+                        object o = new
+                        {
+                            apellidoPaterno = curpData.Apellido1,
+                            apellidoMaterno = curpData.Apellido2,
+                            nombres = curpData.Nombres,
+                            genero = curpData.Sexo,
+                            fechaNacimiento = curpData.FechNac,
+                            nacionalidad = curpData.Nacionalidad,
+                            añoRegistro = curpData.AnioReg,
+                            foja = curpData.Foja,
+                            tomo = curpData.Tomo,
+                            libro = curpData.Libro,
+                            numeroActa = curpData.NumActa,
+                            numeroEntidadRegistro = curpData.NumEntidadReg,
+                            claveMunicipioRegistro = curpData.CveMunicipioReg,
+                            claveEntidadNacimiento = curpData.CveEntidadNac
+                        };
+
+                        return Ok(o);
+                    }
+
+                    if (curpData.StatusOper.ToUpperInvariant() == "NO EXITOSO")
+                    {
+                        object o = new
+                        {
+                            message = curpData.Message
+                        };
+
+                        return Ok(o);
+                    }
+
+                    return NoContent();
+                }
+            }
+            catch
+            {
+                return NotFound("No fue posible validar el curp");
+            }
+
+            return NotFound("No fue posible validar el curp");
+        } //Fin
+
+        /// <summary>
+        /// Obtiene la clave única de registro de población de una persona mediante sus datos personales.
+        /// Usar el método: ObtenerEstadosAcronimo para obtener la cventidadNacimiento
+        /// </summary>
+        /// <param name="renapoRequest">Datos personales</param>
+        /// <returns>CURP de la persona registrada con esos datos</returns>
+        /// <response code="200">Respuesta Valida</response>
+        /// <response code="204">Respuesta Inválida: Sin contenido</response>
+        /// <response code="400">Respuesta Inválida</response>
+        /// <response code="404">Respuesta Inválida: No fue posible obtener el curp</response>
+        [HttpPost("ValidarCurpPorDatos")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> ValidarCurpRenapoDatos([FromBody, Required] CurpByDataRequest renapoRequest)
+        {
+            try
+            {
+                var client = new RestClient($"{_configuration["Renapo:url"]}/consultaCurpPorDetalle")
+                {
+                    Timeout = -1,
+                    UserAgent = agente
+                };
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                var body = renapoRequest.ToJson();
+                request.AddParameter("application/json", body, ParameterType.RequestBody);
+                var response = await client.ExecuteAsync(request);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var curpData = FoByDataOnly.FromJson(response.Content);
+                    if (curpData.StatusOper.ToUpperInvariant() == "EXITOSO")
+                    {
+                        var curpStruct = curpData.CurpStruct[0];
+                        object o = new
+                        {
+                            curp = curpStruct?.Curp,
+                            apellidoPaterno = curpStruct?.Apellido1,
+                            apellidoMaterno = curpStruct?.Apellido2,
+                            nombres = curpStruct?.Nombres,
+                            genero = curpStruct?.Sexo,
+                            fechaNacimiento = curpStruct?.FechNac,
+                            nacionalidad = curpStruct?.Nacionalidad,
+                            añoRegistro = curpStruct?.AnioReg,
+                            foja = curpStruct?.Foja,
+                            tomo = curpStruct?.Tomo,
+                            libro = curpStruct?.Libro,
+                            numeroActa = curpStruct?.NumActa,
+                            numeroEntidadRegistro = curpStruct?.NumEntidadReg,
+                            claveMunicipioRegistro = curpStruct?.CveMunicipioReg,
+                            claveEntidadNacimiento = curpStruct?.CveEntidadNac
+                        };
+
+                        return Ok(o);
+                    }
+
+                    if (curpData.StatusOper.ToUpperInvariant() == "NO EXITOSO")
+                    {
+                        object o = new
+                        {
+                            message = curpData.Message
+                        };
+
+                        return Ok(o);
+                    }
+
+                    return NoContent();
+                }
+            }
+            catch
+            {
+                return NotFound("No fue posible obtener el curp");
+            }
+
+            return NotFound("No fue posible obtener el curp");
+        } //Fin
+
+    }
+}
---
+++ Backend/ValidadorDatos/Controllers/IneController.cs
@@ -0,0 +1,290 @@
+﻿using AngleSharp.Html.Parser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TwoCaptcha.Captcha;
+
+namespace AltergoAPI.Nss.Core.Controllers
+{
+    [SwaggerTag("APIS INE")]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [ApiController]
+    public class IneController : ControllerBase
+    {
+        private const string si_vota = "si-vota-resultado.png";
+        private const string no_vota = "no-vota.jpg";
+        private const string datos_incorrectos = "resultado-datosincorrectos.png";
+        private const string agente = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+
+        private readonly IConfiguration _configuration;
+
+        public IneController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Valida una credencial de elector para votar de tipo C
+        /// </summary>
+        /// <param name="ClaveElector" example="MLVLCR80042609M900">Clave de Elector a 18 caracteres alfanuméricos</param>
+        /// <param name="Ocr" example="0978119955657">Código OCR</param>
+        /// <param name="NumeroEmision" example="00">Número de Emisión de la credencial</param>
+        /// <returns>Si esta vigente y puede votar</returns>
+        /// <response code="200">Respuesta Valida</response>
+        /// <response code="400">Respuesta Inválida: La clave de elector no tiene el formato correcto</response>
+        /// <response code="404">Respuesta Inválida: No fue posible validar la credencial</response>
+        [HttpGet("ValidarIdTipoC")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PostTypeC(
+            [FromQuery, Required] string ClaveElector,
+            [FromQuery, Required] string Ocr,
+            [FromQuery, Required] string NumeroEmision)
+        {
+            if (string.IsNullOrWhiteSpace(ClaveElector))
+            {
+                const string err = "La clave de elector es requerida";
+                return BadRequest(err);
+            }
+
+            if (string.IsNullOrWhiteSpace(Ocr))
+            {
+                const string err = "El Ocr es requerido";
+                return BadRequest(err);
+            }
+
+            if (string.IsNullOrWhiteSpace(NumeroEmision))
+            {
+                const string err = "El número de emisión es requerido";
+                return BadRequest(err);
+            }
+
+            const string clvPattern = @"^([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([A-Z&]|[a-z&]{1})([0-9]{8})([HM]|[hm]{1})([0-9]{3})$";
+            var rgPattern = Regex.Match(ClaveElector, clvPattern, RegexOptions.IgnoreCase);
+            if (!rgPattern.Success)
+            {
+                const string err = "La clave de elector no tiene el formato correcto";
+                return BadRequest(err);
+            }
+
+            const string ocrPattern = @"^([0-9]{13})$";
+            var rgOcrPattern = Regex.Match(Ocr, ocrPattern, RegexOptions.IgnoreCase);
+            if (!rgOcrPattern.Success)
+            {
+                const string err = "El ocr no tiene el formato correcto";
+                return BadRequest(err);
+            }
+
+            var solver = new TwoCaptcha.TwoCaptcha(_configuration["Ine:recaptchatoken"]);
+            var bal = solver.Balance().Result;
+            if (bal <= 0)
+            {
+                const string err = "No hay balance para resolver captcha";
+                return BadRequest(err);
+            }
+
+            var captcha = new ReCaptcha();
+            captcha.SetSiteKey(_configuration["Ine:sitetoken"]);
+            captcha.SetUrl(_configuration["Ine:url"]);
+
+            solver.Solve(captcha).Wait();
+            var gresponse = captcha.Code;
+            var restClient = new RestClient(_configuration["Ine:urlresultado"])
+            {
+                Timeout = -1
+            };
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.AddParameter("g-recaptcha-response", gresponse);
+            request.AddParameter("modelo", "c");
+            request.AddParameter("numeroEmision", NumeroEmision);
+            request.AddParameter("ocr", Ocr);
+            request.AddParameter("claveElector", ClaveElector);
+
+            restClient.UserAgent = agente;
+            var response = await restClient.ExecuteAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = response.Content;
+                var parser = new HtmlParser();
+                var document = parser.ParseDocument(response.Content);
+                var strongs = document.QuerySelectorAll("tbody tr")
+                    .Select(x => new
+                    {
+                        Data = x.TextContent
+                    });
+
+                var details = new Dictionary<string, string>();
+                foreach (var s in strongs)
+                {
+                    var row = s.Data.Replace("\t", "").Trim().Split("\n");
+                    details.Add(row[0], row[1]);
+                }
+
+                var motivo = string.Empty;
+                var flag = false;
+                if (result.Contains(si_vota))
+                {
+                    flag = true;
+                }
+                else if (result.Contains(datos_incorrectos))
+                {
+                    motivo = "Datos incorrectos";
+                }
+                else if (result.Contains(no_vota))
+                {
+                    motivo = "Credencial vencida";
+                }
+
+                object o = new
+                {
+                    estaVigente = flag ? "Si" : "No",
+                    details,
+                    motivo
+                };
+
+                return Ok(o);
+            }
+
+            return NotFound("No fue posible validar la credencial");
+        }//Fin
+
+        /// <summary>
+        /// Valida una credencial de elector para votar de tipo E
+        /// </summary>
+        /// <param name="Cic" example="205147577">Código de identificación de la credencial</param>
+        /// <param name="IdCiudadano" example="027305775">Identificador ciudadano</param>
+        /// <returns>Si esta vigente y puede votar</returns>
+        /// <response code="200">Respuesta Valida</response>
+        /// <response code="400">Respuesta Inválida</response>
+        /// <response code="404">Respuesta Inválida: No fue posible validar la credencial</response>
+        [HttpGet("ValidarIdTipoE")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PostTypeE(
+            [FromQuery, Required] string Cic,
+            [FromQuery, Required] string IdCiudadano)
+        {
+            if (string.IsNullOrWhiteSpace(Cic))
+            {
+                const string err = "Los datos del cic son requeridos";
+                return BadRequest(err);
+            }
+
+            if (string.IsNullOrWhiteSpace(IdCiudadano))
+            {
+                const string err = "Los datos del id ciudadano son requeridos";
+                return BadRequest(err);
+            }
+
+            const string clvPattern = @"^([0-9]{9})$";
+            var rgPattern = Regex.Match(Cic, clvPattern, RegexOptions.IgnoreCase);
+            if (!rgPattern.Success)
+            {
+                const string err = "El cic no tiene el formato correcto";
+                return BadRequest(err);
+            }
+
+            const string ocrPattern = @"^([0-9]{9})$";
+            var rgOcrPattern = Regex.Match(IdCiudadano, ocrPattern, RegexOptions.IgnoreCase);
+            if (!rgOcrPattern.Success)
+            {
+                const string err = "El id ciudadano no tiene el formato correcto";
+                return BadRequest(err);
+            }
+
+            var solver = new TwoCaptcha.TwoCaptcha(_configuration["Ine:recaptchatoken"]);
+            var bal = solver.Balance().Result;
+            if (bal <= 0)
+            {
+                const string err = "No hay balance para resolver captcha";
+                return BadRequest(err);
+            }
+
+            var captcha = new ReCaptcha();
+            captcha.SetSiteKey(_configuration["Ine:sitetoken"]);
+            captcha.SetUrl(_configuration["Ine:url"]);
+
+            solver.Solve(captcha).Wait();
+            var gresponse = captcha.Code;
+            var restClient = new RestClient(_configuration["Ine:urlresultado"])
+            {
+                Timeout = -1
+            };
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.AddParameter("g-recaptcha-response", gresponse);
+            request.AddParameter("modelo", "e");
+            request.AddParameter("cic", Cic);
+            request.AddParameter("idCiudadano", IdCiudadano);
+
+            restClient.UserAgent = agente;
+            var response = await restClient.ExecuteAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var result = response.Content;
+
+                //buscar aqui en el dom
+                var parser = new HtmlParser();
+                var document = parser.ParseDocument(response.Content);
+                var strongs = document.QuerySelectorAll("tbody tr")
+                    .Select(x => new
+                    {
+                        Data = x.TextContent
+                    });
+
+                var details = new Dictionary<string, string>();
+                foreach (var s in strongs)
+                {
+                    var row = s.Data.Replace("\t", "").Trim().Split("\n");
+                    details.Add(row[0], row[1]);
+                }
+
+                var motivo = string.Empty;
+                var flag = false;
+                if (result.Contains(si_vota))
+                {
+                    flag = true;
+                }
+                else if (result.Contains(datos_incorrectos))
+                {
+                    motivo = "Datos incorrectos";
+                }
+                else if (result.Contains(no_vota))
+                {
+                    motivo = "Credencial vencida";
+                }
+
+                object o = new
+                {
+                    estaVigente = flag ? "Si" : "No",
+                    details,
+                    motivo
+                };
+
+                return Ok(o);
+            }
+
+            return NotFound("No fue posible validar la credencial");
+        }//Fin
+
+    }
+}
