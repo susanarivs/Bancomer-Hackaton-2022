c653d29d4b51039ef9428855ef37050b5c5bb211
Carlos Baez <jbaez@outlook.com>
2022-10-15 17:55:03 -0500

    Estructura de la base de datos
    propuesta

---
+++ Backend/Prospectos/TblCanalesProspectos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TblCanalesProspectos]
+(
+    [CanalId] INT NOT NULL,
+    [ProspectoId] INT NOT NULL,
+	CONSTRAINT Cmp_Llave PRIMARY KEY (CanalId, ProspectoId),
+	CONSTRAINT Cmp_FK_Canal FOREIGN KEY (CanalId) REFERENCES TblCanales(Id),
+	CONSTRAINT Cmp_FK_Prospecto FOREIGN KEY (ProspectoId) REFERENCES TblProspectos(Id)
+)
